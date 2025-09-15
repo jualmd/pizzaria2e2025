ee7efd6cf5c0391e86748c483c5894ecfb8d688e
jualmd <rm95207@estudante.fieb.edu.br>
2025-09-15 10:55:47 +0000

    listagem de produtos

---
+++ src/pages/produtos/index.jsx
@@ -0,0 +1,17 @@
+const Produtos = () => {
+
+    return (
+    <> 
+     <h3>Listagem de Produtos</h3>
+      <ul>
+        <li>Pizza de Mussarela</li>
+        <li>Pizza de Calabresa</li>
+        <li>Pizza de Baiana</li>
+        <li>Pizza de Portuguesa</li>
+      </ul>
+    </>
+    )
+
+}
+
+export default Produtos 
