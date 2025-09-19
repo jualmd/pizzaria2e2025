324891a72f1e13c1e523ef7a7ee3f06a0a7ff0ac
jualmd <rm95207@estudante.fieb.edu.br>
2025-09-19 11:06:00 -0300

    consumo

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,40 +1,22 @@
-import {axios} from 'axios'
+import axios from 'axios'
 import { useEffect, useState } from 'react';
-
+ 
 const Produtos = () => {
-    
+    const [pizzas, setPizzas] = useState([])
+ 
     axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto")
-    .then ((response)=>{
-        alert(JSON.stringify(response))
-    })
-
-    // Consumir os produtos do endpoint(rota) 
-    useEffect = (() => { 
-
-
-    }, []);
-
-    // Objetos que possui lista de pizzas
-    const pizzas = [
-        "Pizza de Mussarela",
-        "Pizza de Calabresa",
-        "Pizza de Baiana",
-        "Pizza de Portuguesa",
-        "Pizza de Atum", "Pizza de Seis Queijos"
-    ];
-
+    .then(response=>setPizzas(response.data.data))
+ 
     // Mapeamento das pizzas da lista (iteração)
-    const listaPizzas = pizzas.map(pizza => <li>{pizza}</li>);
-
-    return (
-    <> 
-     <h3>Listagem de Produtos</h3>
-      <ul>
-        {listaPizzas}
-      </ul>
+    const listaPizzas = pizzas.map(pizza =>
+                    <li key={pizza.id}>{pizza.nome}</li>);
+    return(
+    <>
+        <h3>Listagem de Produtos</h3>
+        <ul>
+            {listaPizzas}
+        </ul>
     </>
     )
-
 }
-
-export default Produtos 
+export default Produtos
